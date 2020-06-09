c3341e2667e5558139c759bf5009651a5120f525
devanandp <deva051195@gmail.com>
2020-06-09 12:32:37 +0530

    Added Environment variuable Configurations

--- app.ts
+++ app.ts
@@ -2,9 +2,12 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const app = new Application();
-config();
+let port: string|undefined = Deno.env.get("PORT");
+let portnumber: number;
+if(port!= undefined) {portnumber = +(port);
+    app.use((ctx) => {
+        ctx.response.body = "Hello World!";
+      });
+      await app.listen({ "port": portnumber});
+}
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
-});
-await app.listen({ port: 8000});
