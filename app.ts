16a369074ea97d73b9fd57bb02555fd2e7e57a0c
devanandp <deva051195@gmail.com>
2020-06-09 12:55:55 +0530

    Imported Load package for Getting Root level compile time env variables

--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
 
 const app = new Application();
-let port: string|undefined = Deno.env.get("PORT");
+let port: string | undefined = Deno.env.get('PORT');
+console.log(port);
 let portnumber: number;
-if(port!= undefined) {portnumber = +(port);
-    app.use((ctx) => {
-        ctx.response.body = "Hello World!";
-      });
-      await app.listen({ "port": portnumber});
+if (port != undefined) {
+  portnumber = +(port);
+  app.use((ctx) => {
+    ctx.response.body = "Hello World!";
+  });
+  await app.listen({ "port": portnumber });
 }
-
